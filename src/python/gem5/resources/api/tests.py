1e64a36f865cde01ff6a4c0732b57590579599fe
Harshil2107 <harshilp2107@gmail.com>
2023-05-06 15:58:39 -0700

    edited test for api

--- src/python/gem5/resources/api/tests.py
+++ src/python/gem5/resources/api/tests.py
@@ -3,18 +3,19 @@ from jsonclient import JSONClient
 
 
 resource_id = "x86-ubuntu-18.04-img"
-version = "1.0.0"
+version = "1.1.0"
 
 mongo = MongoClient(
     "mongodb+srv://gem5_user:gem5_user@gem5-vision.wp3weei.mongodb.net/?retryWrites=true&w=majority",
     "gem5-vision",
     "resources")
-# json = JSONClient("C:/Users/hello/Downloads/kiwi.json")
+json = JSONClient(
+    "https://raw.githubusercontent.com/Gem5Vision/json-to-mongodb/unwrap-fields/kiwi1.json")
 
 # print(json.get_resource_obj(resource_id))
 # print(mongo.get_resource_obj(resource_id))
-# jsonObj = json.get_resource_obj(resource_id, version)
+jsonObj = json.get_resource_obj(resource_id, version)
 mongoObj = mongo.get_resource_obj(resource_id, version)
-# print(jsonObj)
+print(jsonObj)
 print(mongoObj)
-# print(jsonObj == mongoObj)
+print(jsonObj == mongoObj)
